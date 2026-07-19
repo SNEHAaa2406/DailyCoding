63ef3c774d58ebb18ab18d3d1ea7441c72701997
Sneha mulik <snehamulik167@gmail.com>
2026-07-19 20:30:57 +0530

    Add files via upload

---
+++ mutable&immutable example.py
@@ -0,0 +1,13 @@
+# a=["sneha","mammma","papa"]
+# print(type(a))
+# a[1]="didi"
+# print(a)
+
+#[] Represents list , it can be modified so we call it mutable.
+
+s=("abc","def","nop")
+print(type(s))
+s[0]="sneha"
+print(s)
+
+# () Represents a tuple , it can't be modified so we call it immutable
---
+++ password.py
@@ -0,0 +1,7 @@
+print("To open this application ")
+a=str(input("Enter your password"))
+if (a=="sneha"):
+    print("Correct Password  ")
+else:
+    print("incorrect password")
+
